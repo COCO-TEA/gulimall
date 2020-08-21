223bcc51649d5784118685e41d17f5807a78696d
September <954670268@qq.com>
2020-08-21 18:04:32 +0800

    项目初始结构创建

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/SpuInfoDescDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.product.dao;
+
+import com.atguigu.gulimall.product.entity.SpuInfoDescEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu信息介绍
+ * 
+ * @author coco
+ * @email coco@gmail.com
+ * @date 2020-08-21 16:16:47
+ */
+@Mapper
+public interface SpuInfoDescDao extends BaseMapper<SpuInfoDescEntity> {
+	
+}
